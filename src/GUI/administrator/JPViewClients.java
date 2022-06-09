28198ef93fc26c9e44dee0eaecf61b7756442d39
Brayan Vargas <103152474+BrayanAntonio08@users.noreply.github.com>
2022-06-09 11:47:13 -0600

    Eliminar los import no usados

--- src/GUI/administrator/JPViewClients.java
+++ src/GUI/administrator/JPViewClients.java
@@ -1,24 +1,16 @@
 package GUI.administrator;
 
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
-import javax.swing.JTextArea;
 
 import Business.UserBusiness;
 import Domain.Client;
